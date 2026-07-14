f0597300724f6c8ccd6451ed03a4595cfa213069
matthew newbould <mjn.97@outlook.com>
2026-07-14 14:26:18 +0100

    added basic routes for sign up and login page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from lib.database_connection import DatabaseConnection
 from lib.users import User
 from lib.user_repository import UserRepository
@@ -7,6 +7,7 @@ from lib.listing import Listing
 from lib.listing_repository import ListingRepository
 # Create a new Flask app
 app = Flask(__name__)
+app.secret_key = "some_really_secret_key"
 
 # == Your Routes Here ==
 
@@ -14,13 +15,19 @@ app = Flask(__name__)
 # Returns the homepage
 # Try it:
 #   ; open http://localhost:5001/index
+
+"""
+Landing page GET(get landing page) POST(create listig) POST(remove listing)
+"""
+
+
 @app.route('/', methods=['GET'])
 def get_landing_page():
     connection = DatabaseConnection()
     connection.connect()
     lising_repository = ListingRepository(connection)
     listing = lising_repository .all()
-    return render_template('index.html', listing=listing)
+    return render_template('static.html', listing=listing)
 
 @app.route('/', methods=['POST'])
 def create_listing():
@@ -42,10 +49,51 @@ def remove_listing():
     listing_repository.remove(listing)
     return redirect("/")    
 
-@app.route('/listings', methods=['GET'])
-def get_listings_page():
-    return render_template('static.html')
 
+"""
+Sign up page GET(get signin page) POST(signup user)
+"""
+
+
+@app.route("/users/new", methods=["GET"])
+def get_sign_up_page():
+    return render_template('signup.html')
+
+@app.route('/users', methods=['POST'])
+def signup_user():
+    connection = DatabaseConnection()
+    connection.connect()
+    user_repository = UserRepository(connection)
+    user_details = request.form
+    new_user = User(name=user_details["name"], email=user_details["email"], phone_number=user_details["phone_number"], password=user_details["password"])
+    user_repository.create(new_user)
+    return redirect("/login")
+
+"""
+Login Page GET(get login page) POST(create session)
+"""
+
+
+@app.route("/users/login", methods=["GET"])
+def get_login_page():
+    return render_template('login.html')
+
+
+@app.route('/sessions', methods=['POST'])
+def create_session():
+    connection = DatabaseConnection()
+    connection.connect()
+    user_repository = UserRepository(connection)
+    name = request.form["name"]
+    password = request.form["password"]
+    user = user_repository.find_by_name(name)
+
+    if user and user.password == password:
+        session["user_id"] = user.id
+        session["name"] = user.name
+        return redirect("/listings")
+    else:
+        return redirect("/users/login")
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
