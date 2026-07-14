e12589a40c111f0857678ae459ed3c28c60b2093
Trouli <99565045+petroulakambouri@users.noreply.github.com>
2026-07-14 16:23:59 +0100

    extra properties in listings.sql

--- seeds/listings.sql
+++ seeds/listings.sql
@@ -15,3 +15,12 @@ INSERT INTO listings (
         (1, 'Rain-soaked shed on a mountain', 'Greenfield', 71),
         (1, 'Uncomfortable camper van in a lay-by', 'Newtown', 21),
         (2, 'Glamorous pad in fancy town', 'Hopington', 311);
+        (2, 'Castle with only three ghosts included', 'Spookingham', 199),
+        (3, 'Luxury treehouse with unreliable ladder', 'Treeford', 89),
+        (1, 'Converted bus stop with panoramic traffic views', 'Roundabout-on-Sea', 34),
+        (4, 'Medieval prison cell', 'Stonechester', 66),
+        (2, 'Studio flat above a loud pub', 'Pintbury', 88),
+        (5, 'Floating house that is not sinking', 'Above Mariana''s Trench', 112),
+        (3, 'Countryside cottage with sheep included', 'Baaxton', 93),
+        (4, 'Tiny house that is not a shed', 'Little Houseton', 68),
+        (5, 'Beach hut only 5 days walk from beach', 'Landlockedshire', 58);
