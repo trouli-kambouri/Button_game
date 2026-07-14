2a6bc2d19308070edc7f355e03f5ec1b3f97af16
Sebastian Block <sebblock88@gmail.com>
2026-07-14 09:57:54 +0100

    Feat: User and UserRepository added - Testing in process but User works

---
+++ lib/users.py
@@ -0,0 +1,7 @@
+class User():
+    def __init__(self, name, email, phone_number, password, id =None):
+        self.name = name
+        self.email = email
+        self.phone_number = phone_number
+        self.password = password
+        self.id = id
