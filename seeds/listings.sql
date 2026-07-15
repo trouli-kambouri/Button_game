a6265a0ccbe2f89cdfa2c15596c039efa1cffce7
Anton <antonedeh1@gmail.com>
2026-07-15 11:34:19 +0100

    LET THERE BE BRANCH

--- seeds/listings.sql
+++ seeds/listings.sql
@@ -14,13 +14,13 @@ INSERT INTO listings (
     VALUES 
         (1, 'Rain-soaked shed on a mountain', 'Greenfield', 71),
         (1, 'Uncomfortable camper van in a lay-by', 'Newtown', 21),
-        (2, 'Glamorous pad in fancy town', 'Hopington', 311);
+        (2, 'Glamorous pad in fancy town', 'Hopington', 311),
         (2, 'Medieval castle with ghost included', 'Spooksville', 199),
         (3, 'Luxury treehouse with unreliable ladder', 'Treeford', 89),
         (1, 'Converted bus stop with panoramic traffic views', 'Roundabout-on-Sea', 34),
-        (4, 'Medieval prison cell', 'Stonechester', 66),
+        (3, 'Medieval prison cell', 'Stonechester', 66),
         (2, 'Studio flat above a loud pub', 'Pintbury', 88),
-        (5, 'Floating house that is not sinking', 'Above Mariana''s Trench', 112),
+        (3, 'Floating house that is not sinking', 'Above Mariana''s Trench', 112),
         (3, 'Countryside cottage with sheep included', 'Baaxton', 93),
-        (4, 'Tiny house that is not a shed', 'Little Houseton', 68),
-        (5, 'Beach hut only 5 days walk from beach', 'Landlockedshire', 58);
+        (3, 'Tiny house that is not a shed', 'Little Houseton', 68),
+        (3, 'Beach hut only 5 days walk from beach', 'Landlockedshire', 58);
