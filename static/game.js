529cb3fa22c7f6e4fae72a7b7cd361b3e5330cf1
Trouli <petroula.kambouri@gmail.com>
2026-09-18 22:20:39 +0100

    so many changes

--- static/game.js
+++ static/game.js
@@ -1,29 +1,36 @@
 
+// INSERTING HTML ELEMENTS
+
+// DEFINING VARIABLES
+
+let i = 0; // number of button clicks
+
+let s = i * 0.1 // seconds // It takes 100 milliseconds to click a button. (I googled it and confirmed by timing my button clicks on average).
+
+let m = s / 60 // minutes
+
+let h = m / 60 // hours
+
+let d = h / 24 // days
+
+let w = d / 7 // weeks
+
+let y = d / 365 // years
+
+let mm = y * 12 // months
 
 
-/*
+// CLICK BUTTON FUNCTION
 
-buttonValue = i
-
-clickButton()
+function clickButton()
 {
-    i++
+    i++;
+    document.getElementById("i").textContent = i;
 }
 
-restartButton()
+// DATE LOG
+
+new Date(year,month,day,hours,minutes,seconds,ms)
 {
-    set buttonValue == 0
+    return "button clicked on + ";
 }
-    
-log for button clicks
-
-title: log
-
-button clicked on: date: time
-
-time spent clicking button: i * 0.1 = number of seconds spent clicking button
-
-
-
-
-*/
